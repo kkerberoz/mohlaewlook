1ad1d6f939ceb0ede0fb760615d435fb8b6f8a2c
kkerberoz-pc <krittaphon.official@gmail.com>
2020-04-16 16:06:57 +0700

    add page employee

--- resources/js/pages/employee/register_emp.vue
+++ resources/js/pages/employee/register_emp.vue
@@ -3,7 +3,7 @@
         <div class="row flex-center full-height">
             <div class="col-md-6">
                 <div class="card">
-                    <div class="card-header bg-info">Register</div>
+                    <div class="card-header bg-info">Register new employee</div>
                     <form v-on:submit="formSubmit">
                         <!-- <form method="post" action="/regis"> -->
                         <input
@@ -108,11 +108,11 @@
                             <span class="form-group">
                                 <label>Email:</label>
                                 <input
-                                    v-model="email"
                                     v-bind:class="{ 'is-invalid': error_email }"
                                     type="email"
                                     class="form-control"
                                     name="email"
+                                    v-model="email"
                                 />
                                 <div class="invalid-feedback">
                                     {{ error_email }}
@@ -122,7 +122,7 @@
 
                         <div class="card-footer">
                             <button type="submit" class="btn btn-block">
-                                Sign up
+                                Register
                             </button>
                         </div>
                     </form>
@@ -131,102 +131,104 @@
         </div>
     </div>
 </template>
+
 <script>
-export default {
-    props: ["csrf", "oldName"],
-    data() {
-        return {
-            username: "",
-            password: "",
-            title: "",
-            name: "",
-            surname: "",
-            DOB: "",
-            email: "",
-            error_username: "",
-            error_password: "",
-            error_title: "",
-            error_name: "",
-            error_surname: "",
-            error_DOB: "",
-            error_email: ""
-        };
-    },
-    methods: {
-        formSubmit(e) {
-            e.preventDefault();
-            let currentObj = this;
-            if (!isNaN(this.username)) {
-                this.error_username = "Please fill your username.";
-                this.errors.push(this.error_username);
-            } else {
-                this.error_username = null;
-            }
+// export default {
+//     props: ["csrf", "oldName"],
+//     data() {
+//         return {
+//             username: "",
+//             password: "",
+//             title: "",
+//             name: "",
+//             surname: "",
+//             DOB: "",
+//             email: "",
+//             error_username: "",
+//             error_password: "",
+//             error_title: "",
+//             error_name: "",
+//             error_surname: "",
+//             error_DOB: "",
+//             error_email: ""
+//         };
+//     },
+//     methods: {
+//         formSubmit(e) {
+//             e.preventDefault();
+//             let currentObj = this;
+//             if (!isNaN(this.username)) {
+//                 this.error_username = "Please fill your username.";
+//                 this.errors.push(this.error_username);
+//             } else {
+//                 this.error_username = null;
+//             }
 
-            if (!isNaN(this.password)) {
-                this.error_password = "Please fill your password.";
-                this.errors.push(this.error_password);
-            } else if (this.password.length < 6) {
-                this.error_password =
-                    "Password has to be at least 6 characters long.";
-                this.errors.push(this.error_password);
-            } else {
-                this.error_password = null;
-            }
+//             if (!isNaN(this.password)) {
+//                 this.error_password = "Please fill your password.";
+//                 this.errors.push(this.error_password);
+//             } else if (this.password.length < 6) {
+//                 this.error_password =
+//                     "Password has to be at least 6 characters long.";
+//                 this.errors.push(this.error_password);
+//             } else {
+//                 this.error_password = null;
+//             }
 
-            if (!isNaN(this.title)) {
-                this.error_title = "Please select your title.";
-                this.errors.push(this.error_title);
-            } else {
-                this.error_title = null;
-            }
-            if (!isNaN(this.name)) {
-                this.error_name = "Please fill your name.";
-                this.errors.push(this.error_name);
-            } else {
-                this.error_name = null;
-            }
-            if (!isNaN(this.surname)) {
-                this.error_surname = "Please fill your surname.";
-                this.errors.push(this.error_surname);
-            } else {
-                this.error_surname = null;
-            }
+//             if (!isNaN(this.title)) {
+//                 this.error_title = "Please select your title.";
+//                 this.errors.push(this.error_title);
+//             } else {
+//                 this.error_title = null;
+//             }
+//             if (!isNaN(this.name)) {
+//                 this.error_name = "Please fill your name.";
+//                 this.errors.push(this.error_name);
+//             } else {
+//                 this.error_name = null;
+//             }
+//             if (!isNaN(this.surname)) {
+//                 this.error_surname = "Please fill your surname.";
+//                 this.errors.push(this.error_surname);
+//             } else {
+//                 this.error_surname = null;
+//             }
 
-            if (!isNaN(this.DOB)) {
-                this.error_DOB = "Please select your Date of Birth.";
-                this.errors.push(this.error_DOB);
-            } else {
-                this.error_DOB = null;
-            }
-            if (!isNaN(this.email)) {
-                this.error_email = "Please fill your E-mail.";
-                this.errors.push(this.error_email);
-            } else {
-                this.error_email = null;
-            }
-            if (!this.errors) {
-                let data = {
-                    username: this.username,
-                    password: this.password,
-                    title: this.title,
-                    name: this.name,
-                    surname: this.surname,
-                    DOB: this.DOB,
-                    email: this.email
-                };
-                axios.post("/api/regis", data).then(response => {
-                    currentObj.output = response.data;
-                    swal.fire(
-                        "Register Success!",
-                        "Cilck the button to continue!",
-                        "success"
-                    ).then(() => {
-                        this.$router.push({ name: "Home" });
-                    });
-                });
-            }
-        }
-    }
-};
+//             if (!isNaN(this.DOB)) {
+//                 this.error_DOB = "Please select your Date of Birth.";
+//                 this.errors.push(this.error_DOB);
+//             } else {
+//                 this.error_DOB = null;
+//             }
+//             if (!isNaN(this.email)) {
+//                 this.error_email = "Please fill your E-mail.";
+//                 this.errors.push(this.error_email);
+//             } else {
+//                 this.error_email = null;
+//             }
+//             if (!this.errors) {
+//                 let data = {
+//                     username: this.username,
+//                     password: this.password,
+//                     title: this.title,
+//                     name: this.name,
+//                     surname: this.surname,
+//                     DOB: this.DOB,
+//                     email: this.email
+//                 };
+//                 axios.post("/api/regis", data).then(response => {
+//                     currentObj.output = response.data;
+//                     swal.fire(
+//                         "Register Success!",
+//                         "Cilck the button to continue!",
+//                         "success"
+//                     ).then(() => {
+//                         this.$router.push({ name: "Home" });
+//                     });
+//                 });
+//             }
+//         }
+//     }
+// };
+//
 </script>
